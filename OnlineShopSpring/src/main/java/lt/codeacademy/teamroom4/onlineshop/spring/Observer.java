50fa0e8103f5203e5040047855d404bb36193d4a
Eimantas <eimantaskiseliovasek@gmail.com>
2023-03-24 14:07:50 +0200

    Added Dummy Data

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/Observer.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/Observer.java
@@ -8,27 +8,82 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Admin;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Customer;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Manager;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.ServiceManager;
 import lt.codeacademy.teamroom4.onlineshop.spring.repositories.AdminRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.CustomerRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ManagerRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ProductRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ServiceRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.services.CustomerService;
+
+import static lt.codeacademy.teamroom4.onlineshop.spring.utils.Roles.*;
+import static lt.codeacademy.teamroom4.onlineshop.spring.utils.Kategorija.*;
 
 @Configuration
 public class Observer {
 	
 	@Autowired
-	AdminRepository adminRepository;
+	private AdminRepository adminRepository;
+	
+	@Autowired
+	private ManagerRepository managerRepository;
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private ServiceRepository serviceRepsitory;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	@EventListener
 	public void seed(ContextRefreshedEvent event) {
 		seedAdmin();
+		seedCustomer();
+		seedServiceManager();
+		seedManager();
+		seedProduct();
 	}
 	
 	private void seedAdmin() {
 		List<Admin> admin = List.of(
-				new Admin("Vilius", "vilius@gmail.com", "svbsdvisbv","svbsdvisbv" ,"Admin")
+				new Admin("ViliusAdmin", "viliusAdmin@gmail.com", "svbsdvisbv","svbsdvisbv" ,ADMIN)
 				);
 		
 		adminRepository.saveAll(admin);
 				
 	}
+	private void seedCustomer() {
+		List<Customer> customer = List.of(
+				new Customer(null, "ViliusCustomer", 0, "viliusCustomer@gmail.com", "svbsdvisbv","svbsdvisbv" ,CUSTOMER, 0)
+				);
+		
+		customerRepository.saveAll(customer);
+	}
+		private void seedServiceManager() {
+			List<ServiceManager> serviceManager = List.of(
+					new ServiceManager( "ViliusServiceManager", 0, "viliusServiceManager@gmail.com", "svbsdvisbv","svbsdvisbv" ,SERVICEMANAGER)
+					);
+			
+			serviceRepsitory.saveAll(serviceManager);
 	
-	
+	}
+		private void seedManager() {
+			List<Manager> manager = List.of(
+					new Manager( "ViliusManager", 0, "viliusManager@gmail.com", "svbsdvisbv","svbsdvisbv" ,SERVICEMANAGER)
+					);
+			
+			managerRepository.saveAll(manager);
+	}	
+		private void seedProduct() {
+			List<Product> product = List.of(
+					new Product("Intel i3", 120,"Quad Core CPU",CPU)
+					);
+			
+			productRepository.saveAll(product);
+			}
 }
--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/utils/Kategorija.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/utils/Kategorija.java
@@ -1,5 +1,5 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.utils;
 
 public enum Kategorija {
-
+CPU, GPU, RAM, HDD, Mainboard, PSU, ThermalPaste,Fans, DesktopComputer, LaptopComputer, AllInOneComputer	
 }
