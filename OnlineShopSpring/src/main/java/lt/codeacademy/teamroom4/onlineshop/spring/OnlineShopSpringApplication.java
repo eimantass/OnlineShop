c31da9a95797ede7053a15bd57a46977fe76e00b
Eimantas <eimantaskiseliovasek@gmail.com>
2023-03-24 08:15:18 +0200

    Initial Project created

---
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/OnlineShopSpringApplication.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.teamroom4.onlineshop.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopSpringApplication.class, args);
+	}
+
+}
