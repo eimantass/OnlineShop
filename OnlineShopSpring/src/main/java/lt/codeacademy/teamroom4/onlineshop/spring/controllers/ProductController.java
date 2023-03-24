f3da69d376aa9004e645f56252623588622100dc
Eimantas <eimantaskiseliovasek@gmail.com>
2023-03-24 14:34:06 +0200

    ControllerFixes

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ProductController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ProductController.java
@@ -13,11 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
 import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ProductRepository;
 
-@Controller
+@RestController
 @RequestMapping("/products")
 
 public class ProductController {
