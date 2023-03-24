fb218bb0574eb50388d1c526e865bb5c1c8690f6
Eimantas <eimantaskiseliovasek@gmail.com>
2023-03-24 10:34:15 +0200

    Expanded Controllers

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/AdminController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/AdminController.java
@@ -1,11 +1,55 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
+import java.net.URISyntaxException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Admin;
 import lt.codeacademy.teamroom4.onlineshop.spring.repositories.AdminRepository;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
 @Autowired
 private AdminRepository adminRepository;
+
+@GetMapping
+public List<Admin> getAdmins() {
+	return adminRepository.findAll();
+}
+
+@GetMapping("/{id}")
+public Admin getAdmin(@PathVariable Long id) {
+	return adminRepository.findById(id).orElseThrow(RuntimeException::new);
+}
+
+@PostMapping
+public Admin createAdmin(@RequestBody Admin admin) throws URISyntaxException {
+	Admin savedAdmin = adminRepository.save(admin);
+	return savedAdmin;
+
+}
+
+@PutMapping("/{id}")
+public Admin updateAdmin(@PathVariable Long id, @RequestBody Admin admin) {
+	Admin currentAdmin = adminRepository.findById(id).orElseThrow(RuntimeException::new);
+	currentAdmin.setFullName(admin.getFullName());
+	currentAdmin.setEmail(admin.getEmail());
+	currentAdmin.setPassword(admin.getPassword());
+	currentAdmin.setMatchingPassword(admin.getMatchingPassword());
+	return adminRepository.save(admin);
+}
+
+@DeleteMapping("/{id}")
+public ResponseEntity<?> deleteAdmin(@PathVariable Long id) {
+	adminRepository.deleteById(id);
+	return ResponseEntity.ok().build();
+}
 }
--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ManagerController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ManagerController.java
@@ -1,15 +1,57 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.net.URISyntaxException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Manager;
 import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ManagerRepository;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ManagerController {
 
-@Autowired
-private ManagerRepository managerRepository;
+	@Autowired
+	private ManagerRepository managerRepository;
+
+	@GetMapping
+	public List<Manager> getManagers() {
+		return managerRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Manager getManagers(@PathVariable Long id) {
+		return managerRepository.findById(id).orElseThrow(RuntimeException::new);
+	}
+
+	@PostMapping
+	public Manager createManagers(@RequestBody Manager manager) throws URISyntaxException {
+		Manager savedManager = managerRepository.save(manager);
+		return savedManager;
+
+	}
+
+	@PutMapping("/{id}")
+	public Manager updateManagers(@PathVariable Long id, @RequestBody Manager manager) {
+		Manager currentManager = managerRepository.findById(id).orElseThrow(RuntimeException::new);
+		currentManager.setFullName(manager.getFullName());
+		currentManager.setEmail(manager.getEmail());
+		currentManager.setPassword(manager.getPassword());
+		currentManager.setMatchingPassword(manager.getMatchingPassword());
+		return managerRepository.save(manager);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteManagers(@PathVariable Long id) {
+		managerRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
 }
