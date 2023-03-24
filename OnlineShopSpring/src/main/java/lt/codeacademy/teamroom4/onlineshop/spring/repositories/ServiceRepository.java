3d8473784c987a5d72692c05de7736e7b4d0432a
Eimantas <eimantaskiseliovasek@gmail.com>
2023-03-24 09:40:56 +0200

    Changed repositories to interface

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ServiceRepository.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ServiceRepository.java
@@ -3,6 +3,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ServiceRepository extends JpaRepository<Service, Long> {
+public interface ServiceRepository extends JpaRepository<Service, Long> {
 
 }
