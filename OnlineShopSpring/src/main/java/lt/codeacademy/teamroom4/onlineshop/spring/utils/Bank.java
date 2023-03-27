902b58921651d9c977c9c44fe28fa03c1a9d116d
Vilius <mano@gmail.lt>
2023-03-27 10:08:20 +0300

    Bank

---
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/utils/Bank.java
@@ -0,0 +1,20 @@
+package lt.codeacademy.teamroom4.onlineshop.spring.utils;
+
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Customer;
+
+public class Bank {
+	
+	void pay(Customer cust, double pay) {
+		double balance = cust.getMoney();
+		String name = cust.getFullName();
+		if(balance >= pay) {
+			System.out.println(name + " pay " + pay);
+			
+			balance = balance - pay;
+			System.out.println("Your balance after pay: " + balance);
+		}else {
+			System.out.println(name + " you can not pay " + pay);
+			System.out.println("Your balance is: " + balance);
+		}
+	}
+}
