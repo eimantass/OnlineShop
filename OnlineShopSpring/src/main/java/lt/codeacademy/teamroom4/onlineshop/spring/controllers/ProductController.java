9cf01c980ff6856096a29a307fbecf26accefef5
Dmitrij <dimman93@bk.ru>
2023-03-28 14:02:31 +0300

    Added prekes function page, but doesnt work because of security

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ProductController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class ProductController {
 	private ProductService productService;
 	
 	@GetMapping("/all")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public String index(ModelMap modelMap) {
 		modelMap.put("product", productService.findAll());
 		return "products/all";
