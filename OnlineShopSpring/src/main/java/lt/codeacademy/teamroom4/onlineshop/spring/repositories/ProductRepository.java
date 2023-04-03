48e85df452585cecfaa730096625a487b8dddc36
Vilius <mano@gmail.lt>
2023-04-03 14:27:05 +0300

    Search

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ProductRepository.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	List<Product> findByNameContainingIgnoreCase(String name);
 }
