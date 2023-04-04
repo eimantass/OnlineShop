15fb54c971cb689266c936a85e5176990e7d3096
Dmitrij <dimman93@bk.ru>
2023-04-04 08:55:33 +0300

    Small file adjustments and move homepage mainBody to separate component

---
+++ online-shop-react/src/components/MainBody.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MainBody() {
+  return (
+    <main>
+          <h2 className="center">Test Text1</h2>
+  
+          <h1 className="center">Text2</h1>
+  
+          <h1 className="center">Text3</h1>
+  
+        </main>
+   
+  );
+}
+
+export default MainBody;
--- online-shop-react/src/components/categories-menu.js
+++ online-shop-react/src/components/categories-menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './css/Body.css';
+import './css/categoriesMenu.css';
 
 function Categories() {
   return (
