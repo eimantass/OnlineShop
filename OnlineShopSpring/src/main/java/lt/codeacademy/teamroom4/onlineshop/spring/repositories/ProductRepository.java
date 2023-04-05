dd1d8ef899212dd94a8b204aeb84b285d072d147
Eimantas <eimantaskiseliovasek@gmail.com>
2023-04-05 11:23:09 +0300

    Updated ProductService

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ProductRepository.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ProductRepository.java
@@ -3,9 +3,15 @@ package lt.codeacademy.teamroom4.onlineshop.spring.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
-
+import lt.codeacademy.teamroom4.onlineshop.spring.utils.Parameters.Brands;
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	List<Product> findByNameContainingIgnoreCase(String name);
+	
+	@Query("SELECT DISTINCT brand FROM Product")
+	List<Brands> findAllBrandsDistincts();
 }
