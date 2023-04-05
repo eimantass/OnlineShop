406c8d1d37a91d9463792552c4ed3a2acb08e024
Eimantas <eimantaskiseliovasek@gmail.com>
2023-04-05 08:54:03 +0300

    Brought back ProductService functions

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ProductRepository.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/repositories/ProductRepository.java
@@ -3,9 +3,14 @@ package lt.codeacademy.teamroom4.onlineshop.spring.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
+import lt.codeacademy.teamroom4.onlineshop.spring.utils.Parameters.Brands;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	List<Product> findByNameContainingIgnoreCase(String name);
+	
+	@Query("SELECT DISTINCT brand FROM Product")
+	List<Brands> findAllBrandsDistincts();
 }
