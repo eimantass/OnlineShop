48f206a93c36af5f698b9db36fc3308b97faad25
Laury <laurynasdavi@gmail.com>
2023-04-05 08:55:38 +0300

    Footer atsiranda puslapio apačioje, net jei nėra duomenu užpildyti visą puslapį

--- online-shop-react/src/index.js
+++ online-shop-react/src/index.js
@@ -5,12 +5,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import FooterMenu from "./components/FooterMenu";
 
+import './App.css';
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
+  <div className="FooterPlacer">
   <BrowserRouter>
     <App />
     <FooterMenu/>
   </BrowserRouter>
+  </div>
 );
