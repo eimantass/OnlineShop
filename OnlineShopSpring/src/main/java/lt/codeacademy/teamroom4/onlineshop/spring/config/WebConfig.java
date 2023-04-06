5adc1486b915938b9d98fe29e561012828aab23b
Eimantas <eimantaskiseliovasek@gmail.com>
2023-04-06 10:38:04 +0300

    Commented WebConfig

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/config/WebConfig.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/config/WebConfig.java
@@ -5,14 +5,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
  //Here mapping for crud operations is configured
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/products/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(false)
-                .maxAge(3600);
-    }
-}
+	/*
+	 * @Configuration public class WebConfig implements WebMvcConfigurer {
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/products/**") .allowedOrigins("http://localhost:3000")
+	 * .allowedMethods("GET", "POST", "PUT", "DELETE") .allowCredentials(false)
+	 * .maxAge(3600); } }
+	 */
--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/entities/User.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/entities/User.java
@@ -50,7 +50,7 @@ public class User {
 	
 	private double money;
 	
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@ManyToMany(cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
 	   inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
