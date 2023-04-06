f4f1fdf4c32fdd5e2ebde74879b27e4b0b9dae9e
Vilius <mano@gmail.lt>
2023-04-06 14:41:47 +0300

    WishList

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/GeneralCartController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/GeneralCartController.java
@@ -7,8 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.ShoppingCart;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.WishList;
 import lt.codeacademy.teamroom4.onlineshop.spring.services.CartService;
 import lt.codeacademy.teamroom4.onlineshop.spring.services.ProductService;
+import lt.codeacademy.teamroom4.onlineshop.spring.services.WishListService;
 //This class shows the users shopping cart or creates empty one if user does not have one.
 @ControllerAdvice
 public class GeneralCartController {
@@ -19,12 +21,22 @@ public class GeneralCartController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private WishListService wishListService;
+	
 	public void populateModel(Model model, HttpServletRequest request) {
-		String sessionToken = (String) request.getSession(false).getAttribute("sessionToken");
+		String sessionToken = (String) request.getSession(true).getAttribute("sessionToken");
+		String sessionTokenWishList = (String) request.getSession(true).getAttribute("sessionTokenWishList");
 		if(sessionToken == null) {
 			model.addAttribute("shoppingCart", new ShoppingCart());
 		}else {
 			model.addAttribute("shoppingCart",cartService.getShoppingCartBySessionToken(sessionToken));
+		}
+		
+		if(sessionTokenWishList == null) {
+			model.addAttribute("wishList", new WishList());
+		}else {
+			model.addAttribute("wishList",wishListService.getWishListBySessionToken(sessionTokenWishList));
 		}
 		
 		model.addAttribute("categories", productService.getAllCategories());
