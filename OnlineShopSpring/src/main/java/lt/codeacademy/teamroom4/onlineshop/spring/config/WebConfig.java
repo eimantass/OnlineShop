72357916e4eb09d60d3587a9fa2f85d67d5db5bf
Dmitrij <dimman93@bk.ru>
2023-04-11 15:02:27 +0300

    Add additional webSecurity functions to control CORS

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/config/WebConfig.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/config/WebConfig.java
@@ -6,10 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
  //Here mapping for crud operations is configured
 //	
-//	  @Configuration public class WebConfig implements WebMvcConfigurer {
-//	  
-//	  @Override public void addCorsMappings(CorsRegistry registry) {
-//	  registry.addMapping("/products/**") .allowedOrigins("http://localhost:3000")
-//	  .allowedMethods("GET", "POST", "PUT", "DELETE") .allowCredentials(false)
-//	  .maxAge(3600); } }
-	
+@Configuration public class WebConfig implements WebMvcConfigurer {
+	  
+@Override 
+public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")  // Update the mapping to "/**" to allow all paths
+		.allowedOrigins("http://localhost:8081")
+		.allowedMethods("GET", "POST", "PUT", "DELETE") .allowCredentials(false)
+		.maxAge(3600)
+		.allowedHeaders("*") // Add this line to allow all headers
+		.exposedHeaders("*"); // Add this line to expose all headers} 
+
+}
+}
