4c5336bd99e705584159e767007395d8b895a55d
Dmitrij <dimman93@bk.ru>
2023-04-11 10:30:30 +0300

    Edit brands for products now works

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/utils/Parameters/Brands.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/utils/Parameters/Brands.java
@@ -1,8 +1,12 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.utils.Parameters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Brands {
 AMD,
 NVIDIA,
 INTEL,
-GOODRAM
+GOODRAM;
+
 }
