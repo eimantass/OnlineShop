71aa447d54a0e4a0c43b042d9da5812ac14655de
Vilius <mano@gmail.lt>
2023-04-14 09:49:11 +0300

    Testai ir cart

---
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/services/CartServiceImpl.java
@@ -0,0 +1,62 @@
+package lt.codeacademy.teamroom4.onlineshop.spring.services;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Cart;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.CartItem;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ShoppingCartRepository;
+
+@Service
+public class CartServiceImpl implements CartService {
+	
+	@Autowired 
+	ShoppingCartRepository repository;
+
+	@Override
+	public Cart createCart() {
+		Cart cart = new Cart();
+		return repository.save(cart);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Cart addItem(Long id, CartItem item) {
+		Cart cart = repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Cart not found"));
+		//item.setCart(cart);
+		cart.getItems().addAll((Collection<? extends Product>) item);
+		return repository.save(cart);
+	}
+
+	@Override
+	public Cart removeItem(Long id, CartItem item) {
+		Cart cart = repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Cart not found"));
+		cart.getItems().remove(item);
+		return repository.save(cart);
+	}
+
+	@Override
+	public Cart updateItemQuantity(Long id, CartItem item, int quantity) {
+		Cart cart = repository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Cart not found"));
+		item.setQuantity(quantity);
+		return repository.save(cart);
+	}
+
+	@Override
+	public void deleteCart(Long id) {
+		repository.deleteById(id);
+	}
+
+	@Override
+	public List<Cart> getAllCarts() {
+		return repository.findAll();
+	}
+
+}
--- OnlineShopSpring/src/test/java/lt/codeacademy/teamroom4/onlineshop/spring/security/jwt/AuthTokenFilterTest.java
+++ OnlineShopSpring/src/test/java/lt/codeacademy/teamroom4/onlineshop/spring/security/jwt/AuthTokenFilterTest.java
@@ -2,13 +2,27 @@ package lt.codeacademy.teamroom4.onlineshop.spring.security.jwt;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 class AuthTokenFilterTest {
 
 	@Test
-	void testDoFilterInternalHttpServletRequestHttpServletResponseFilterChain() {
-		fail("Not yet implemented");
+	void testDoFilterInternalHttpServletRequestHttpServletResponseFilterChain() throws ServletException, IOException {
+		 HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+		 HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+		 FilterChain filterChain = Mockito.mock(FilterChain.class);
+		 Mockito.when(request.getHeader("X-Auth-Token")).thenReturn("myAuthToken");
+		 AuthTokenFilter filter = new AuthTokenFilter();
+		 filter.doFilter(request, response, filterChain);
+		 Mockito.verify(filterChain).doFilter(request, response);
 	}
 
 }
