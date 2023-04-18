5881ba9ff54e00d99ca952be175db1d6491ae196
Vilius <mano@gmail.lt>
2023-04-18 13:45:05 +0300

    get photo

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/PhotoController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/PhotoController.java
@@ -2,22 +2,30 @@ package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
 
 import java.io.IOException;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.Photo;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.PhotoRepository;
 import lt.codeacademy.teamroom4.onlineshop.spring.services.PhotoService;
 
 @RestController
 public class PhotoController {
+	
+	@Autowired
+	PhotoRepository repository;
 	
 	@Autowired
 	private PhotoService photoService;
@@ -34,4 +42,16 @@ public class PhotoController {
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); 
 			} 
 	}
+	
+	 @GetMapping("/{id}")
+	 public ResponseEntity<byte[]> getPhoto(@PathVariable Long id) {
+		 Optional<Photo> photo = repository.findById(id);
+	     if (photo.isPresent()) {
+	    	 byte[] data = photo.get().getData();
+	         HttpHeaders headers = new HttpHeaders();
+	         headers.setContentType(MediaType.IMAGE_JPEG);
+	         return new ResponseEntity<>(data, headers, HttpStatus.OK);
+	     }
+	     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	 }
 }
