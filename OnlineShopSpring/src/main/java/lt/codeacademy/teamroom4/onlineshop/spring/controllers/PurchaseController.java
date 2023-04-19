53f6dea0dda4829112d0c2e5cd9d7dd2525f3999
Eimantas <eimantaskiseliovasek@gmail.com>
2023-04-19 21:17:25 +0300

    Reverted to earlier version

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/PurchaseController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/PurchaseController.java
@@ -1,7 +1,6 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,26 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.CartItem;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Wallet;
-import lt.codeacademy.teamroom4.onlineshop.spring.services.CartServiceImpl;
 import lt.codeacademy.teamroom4.onlineshop.spring.services.PurchaseService;
-import lt.codeacademy.teamroom4.onlineshop.spring.services.WalletService;
 
 @RestController
-@RequestMapping("/purchase/")
+@RequestMapping("/purchase")
 public class PurchaseController {
 	
 	@Autowired
 	PurchaseService purchaseService;
-	@Autowired
-	CartServiceImpl cartItemService;
-	@Autowired
-	WalletService walletService;
-	/*@PostMapping("/buy")
-	public void buyItem(@RequestParam  Long cartItemId, @RequestParam int quantity, @RequestParam double price,@RequestParam  Long walletId) {
-		//Does not work yet.
-		purchaseService.buyItem(cartItemService.findByUserId(cartItemId), quantity, price,walletService.getById(walletId));
-	}
-	*/
+	
 	@PostMapping
 	public void buyItem(@RequestParam CartItem item, @RequestParam int quantity, @RequestParam double price, Wallet wallet) {
 		purchaseService.buyItem(item, quantity, price, wallet);
--- online-shop-react/src/pages/Returns.js
+++ online-shop-react/src/pages/Returns.js
@@ -5,37 +5,7 @@ export default function Returns() {
       <div>
         <Categories/>
         <h1 style={{textAlign: "center"}}>Returns page</h1>
-        {/*----------------------------------------------------------------------------------*/}
-        <h1>RETURNS AND REFUNDS</h1>
-        <h2 style={{ fontWeight: 'bold' }}>30-Day Money-Back Guarantee</h2>
-        <h3>
-          We guarantee your satisfaction with all knivesandtools merchandise.
-          You can always cancel your order at any time for a full refund within 30 days of delivery in accordance with your rights under the Consumer Protection (Distance Selling) Regulations 2000.
-          Only unused items in original packaging may be returned for a full refund.
-        </h3>
-        <h3>
-         In order to return goods please always contact us first at "Email" so we can send you the packing and shipping instructions.
-          We will always refund you the same way you paid us (ie credit card or wire transfer).
-          You will receive the refund within 14 days after we received the return shipment.
-        </h3>
-        <h3>
-          NOTE: The 30-day money back guarantee does not apply to products that have been used and/or are damaged.
-        </h3>
-        {/*----------------------------------------------------------------------------------*/}
-        <h1>LIMITATION OF LIABILITY</h1>
-        <h3>
-        TO THE FULLEST EXTENT PERMITTED AT LAW, "Group name" GROUP IS PROVIDING THIS WEBSITE AND ITS CONTENTS ON AN "AS IS" BASIS AND MAKES NO (AND EXPRESSLY DISCLAIMS ALL) REPRESENTATIONS OR WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, WITH RESPECT TO THIS WEBSITE OR THE INFORMATION, CONTENT, MATERIALS OR PRODUCTS INCLUDED IN THIS SITE INCLUDING, WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-        IN ADDITION, KATO GROUP B.V. DOES NOT REPRESENT OR WARRANT THAT THE INFORMATION ACCESSIBLE VIA THIS WEBSITE IS ACCURATE, COMPLETE OR CURRENT.
-        Prices and availability information is subject to change without notice.
-        </h3>
-        <b2>
-
-        </b2>
-        <h3>
-        Except as specifically stated on this website, to the fullest extent permitted at law, neither "Group name" Group any of its affiliates, directors, employees or other representatives will be liable for damages arising out of or in connection with the use of this website or the information, content, materials or products included on this site.
-        This is a comprehensive limitation of liability that applies to all damages of any kind, including (without limitation) compensatory, direct, indirect or consequential damages, loss of data, income or profit, loss of or damage to property and claims of third parties.
-        For the avoidance of doubt, "Group name" Group does not limit its liability for death or personal injury to the extent only that it arises as a result of the negligence of "Group name" Group, its affiliates, directors, employees or other representatives.
-        </h3>
+        {/* rest of your contact page content */}
       </div>
     );
     
