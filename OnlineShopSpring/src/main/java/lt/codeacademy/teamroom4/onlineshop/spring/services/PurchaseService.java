51cd3737a13a4286b446f01c10a61754b169aad2
Eimantas <eimantaskiseliovasek@gmail.com>
2023-04-20 10:27:00 +0300

    Purchase changes

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/services/PurchaseService.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/services/PurchaseService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Cart;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.CartItem;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Purchase;
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.User;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Wallet;
 import lt.codeacademy.teamroom4.onlineshop.spring.repositories.PurchaseRepository;
 
@@ -15,13 +16,13 @@ public class PurchaseService {
 	@Autowired
 	PurchaseRepository purchaseRepository;
 	
-	public void buyItem(Wallet wallet, Cart cart) {
+	public void buyItem(User user, Cart cart) {
         Purchase purchase = new Purchase();
         // purchase.setItem(item);
         // purchase.setQuantity(quantity);
        
         purchase.setPrice(cart.getTotalPrice());
-        wallet.setCurrentBalance(wallet.getCurrentBalance()-cart.getTotalPrice());
+        user.setMoney(user.getMoney()-cart.getTotalPrice());
         purchaseRepository.save(purchase);
     }
 }
