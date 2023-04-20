cca2ed8cec3079a33568db7c183ef9798ac084a4
Eimantas <eimantaskiseliovasek@gmail.com>
2023-04-20 14:20:20 +0300

    Purchase tweaks

--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/PurchaseController.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/PurchaseController.java
@@ -1,6 +1,7 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +20,13 @@ public class PurchaseController {
 	@Autowired
 	PurchaseService purchaseService;
 	
-	@PostMapping
+	@PostMapping("/buy")
 	public void buyItem(User user, Cart cart) {
 		purchaseService.buyItem(user, cart);
 	}
+	@PostMapping("/buy2/{userId}/{cartId}")
+	public void buyItem2(@PathVariable Long userId,@PathVariable Long cartId) {
+		purchaseService.buyItem(userId, cartId);
+	}
+	
 }
--- OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/services/PurchaseService.java
+++ OnlineShopSpring/src/main/java/lt/codeacademy/teamroom4/onlineshop/spring/services/PurchaseService.java
@@ -9,15 +9,31 @@ import lt.codeacademy.teamroom4.onlineshop.spring.entities.Purchase;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.User;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Wallet;
 import lt.codeacademy.teamroom4.onlineshop.spring.repositories.PurchaseRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.ShoppingCartRepository;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.UserRepository;
 
 @Service
 public class PurchaseService {
 	
 	@Autowired
 	PurchaseRepository purchaseRepository;
-	
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	ShoppingCartRepository cartRepository;
 	public void buyItem(User user, Cart cart) {
         Purchase purchase = new Purchase();
+        // purchase.setItem(item);
+        // purchase.setQuantity(quantity);
+       
+        purchase.setPrice(cart.getTotalPrice());
+        user.setMoney(user.getMoney()-cart.getTotalPrice());
+        purchaseRepository.save(purchase);
+    }
+	public void buyItem(Long userId, Long cartId) {
+        Purchase purchase = new Purchase();
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new RuntimeException("Cart not found"));
         // purchase.setItem(item);
         // purchase.setQuantity(quantity);
        
