e7856ad7a9ffd38826aa11f676cd1743812a1cd5
Vilius <mano@gmail.lt>
2023-04-21 10:02:06 +0300

    isverstas apatinis menu

--- online-shop-react/src/pages/Privacy.js
+++ online-shop-react/src/pages/Privacy.js
@@ -8,42 +8,37 @@ export default function Privacy() {
       <div>
         <Categories/>
         <h1 style={{textAlign: "center"}}>{t('privacy')}</h1>
-        <h1>Privacy statement ThisPageGroup</h1>
-        <h3>In our servers only the data that is essential for purchases and customer services is collected.
-          </h3>
-        <h1>What data do we collect?</h1>
+        <h1>{t('privacyhead1')}</h1>
         <h3>
-        When you order at our website we need data to make sure we can, for instance, complete the payment correctly and ship your package to the right address.
-        Or if we need to reach you if something goes wrong with your order. We also collect cookies, but more about that later.
+        {t('privacyhead2')}
         </h3>
-<h3>These are the personal data we collect:</h3>
+        <h1>{t('privacyhead3')}	</h1>
+        <h3>
+        {t('privacyhead4')}
+        </h3>
+<h3>{t('privacyhead5')}</h3>
 <ul>
-  <li>Phone number</li>
-  <li>Invoice address</li>
-  <li>Email address</li>
-  <li>Payment method</li>
-  <li>Order history</li>
+  <li>{t('privacylist1')}</li>
+  <li>{t('privacylist2')}</li>
+  <li>{t('privacylist3')}</li>
+  <li>{t('privacylist4')}</li>
+  <li>{t('privacylist5')}</li>
 
   <li>
-  Via our web shop we always enable you to ask questions via different channels (such as chat, email, telephone and social media).
-  You are the one to decide which information you provide when you use one of these channels.
+  {t('privacylist6')}
   </li>
 </ul>
 
-<h1>Accessing and changing your data</h1>
+<h1>{t('privacybody1')}</h1>
 <h3>
-If you have any questions regarding our privacy policy or questions about access to, changes made to (or removal of) your personal data,
-you can contact us at any time via the information listed below.
-To prevent abuse we may ask you to identify yourself adequately.
-We want to prevent anyone from having control over your personal information.
-It is best to submit your request by email.
+{t('privacybody2')}
 </h3>
 <b>
-Want to talk to an actual human being? Please contact our customer service department on working days at "Number"
+{t('privacybody3')}
 </b>
 <h1>  </h1>
 <b>
-Prefer to send us an email? Send an email to "Email".
+{t('privacybody4')}
 </b>
 
       </div>
--- online-shop-react/src/pages/Warranty.js
+++ online-shop-react/src/pages/Warranty.js
@@ -7,18 +7,15 @@ export default function Warranty() {
       <div>
         <Categories/>
         <h1 style={{textAlign: "center"}}>{t('warranty')}</h1>
-        <h1>Damaged or defective items</h1>
+        <h1>{t('warrantyhead1')}</h1>
         <h3>
-          If you receive a damaged or defective item, we will promptly send you a replacement or issue you a full refund after you have returned the damaged or defective product.
-          You will not be charged any additional shipping or handling fees for replacement shipments.
+        {t('warrantybody1')}
         </h3>
         <h3>
-        All items on our website come with at least a 1-year warranty, unless specified otherwise.
+        {t('warrantybody2')}
         </h3>
         <h3>
-         In order to return goods please always contact us first at "Email" so we can send you the packing and shipping instructions.
-          We will always refund you the same way you paid us (ie credit card or wire transfer).
-          You will receive the refund within 14 days after we received the return shipment.
+        {t('warrantybody3')}
         </h3>
       </div>
     );
