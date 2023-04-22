3ceee6a8c9b6596d596696d2bf768993ddd46c1e
Laury <laurynasdavi@gmail.com>
2023-04-22 19:26:08 +0300

    Apnaujintas searchpage.js, ir Filt en.json ir lt.json

--- online-shop-react/src/layout/searchpage.js
+++ online-shop-react/src/layout/searchpage.js
@@ -4,24 +4,40 @@ import React, { useState, useEffect,} from 'react';
 import './css/Customers.css';
 import { useNavigate } from "react-router-dom";
 import ProductService from "../services/product.service";
+import WishListService from "../services/wishlist.service";
+import CartService from "../services/cart.service";
+import {useTranslation} from 'react-i18next';
 
 
 
 const SearchpageT = () => {
-
   const {val} = useParams();
   const [products, setProducts] = useState([]);
   const [productsNEW, setProductsNEW] = useState([]);
+  const [wishlistMessage, setwishlistMessage] = useState("");
+  const [cartMessage, setCartMessage] = useState("");
+  const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await ProductService.getAllProducts();
-        setProducts(response.data);
+        const productsWithQuantity = response.data.map(product => ({
+          ...product,
+          selectedQuantity: product.quantity > 0 ? 1 : 0
+        }));
+        setProducts(productsWithQuantity);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
+  }, []);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    setCurrentUser(user);
   }, []);
 
   useEffect(() => {
@@ -37,29 +53,127 @@ const SearchpageT = () => {
     || products.category.includes(query)
     || products.brand.includes(query));
 
-    //console.log("updateList worked");         
+    //console.log("updateList worked");    
 
     setProductsNEW(updatedList);
   //console.log("set new product")
   };
 
-  return (
-<main>
-      <h2> </h2>
-      <h2 className="center">Products List containing {val}:</h2>
-      {/* Products List from Repository */}
-      {/*<FilterItem/>*/}
+  const handleAddToWishlist = async (product) => {
+    if (!currentUser) {
+      // Redirect the user to the login page if not logged in
+      navigate("/login");
+      return;
+    }
 
-      <ul>
+    try {
+      // Get all wishlists of the user
+      const wishlists = await WishListService.getAllWishListsByUserId(currentUser.id);
+      // If user doesnt have a wishlist - create it
+      if (!wishlists || wishlists.length === 0) {
+        await WishListService.createWishListByUserId(currentUser.id);
+        setwishlistMessage("Wish List created successfully!");
+      } else {
+        setwishlistMessage("You already have an active wishlist!");
+      }
+      // Check again for wishlists as per userID
+      let wishlists2 = await WishListService.getAllWishListsByUserId(currentUser.id);
+
+      // Sort the wishlists by createdAt field in descending order
+      wishlists2 = wishlists2.sort((a, b) => {
+        if (a.createdAt > b.createdAt) return -1;
+        if (a.createdAt < b.createdAt) return 1;
+        return 0;
+      });
+      // We select the latest wishlist we get from wish list of the user and assign it
+      let wishlist;
+      wishlist = wishlists2[0];
+
+      // Construct the data to be sent in the request body
+      const data = {
+        productId: product.id,
+        cartId: wishlist.id
+      };
+
+      console.debug("Sending JSON Wishlist: ", data);
+
+      await WishListService.addItemToWishList(wishlist.id, product.id);
+
+      alert(`Product ${product.name} has been added to your Wish List.`);
+    } catch (error) {
+      console.log(error);
+      alert("Failed to add the product to your Wish List.");
+    }
+  };
+
+  const handleAddToCart = async (product) => {
+    if (!currentUser) {
+      navigate("/login");
+      return;
+    }
+  
+    const confirmed = window.confirm(`Are you sure you want to add ${product.name} to your cart?`);
+  
+    if (!confirmed) {
+      return;
+    }
+  
+    try {
+      const currentUser = JSON.parse(localStorage.getItem('user'));
+      const carts = await CartService.GetActiveCarts(currentUser.id);
+  
+      let cart;
+      let cartMessage;
+  
+      if (!carts || carts.length === 0) {
+        cart = await CartService.createCartByUserId(currentUser.id);
+        cartMessage = "Cart created successfully!";
+      } else {
+        carts.sort((a, b) => b.createdAt - a.createdAt);
+        cart = carts[0];
+        cartMessage = "You already have an active cart!";
+      }
+  
+      await CartService.addItemToCart(cart.id, product.id, product.selectedQuantity);
+  
+      alert(`Product ${product.name} has been added to your cart.`);
+      console.debug("Sending JSON Cart: ", cart.id, product.id, product.selectedQuantity);
+  
+      setCartMessage(cartMessage);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleQuantityChange = (product, newQuantity) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((prevProduct) =>
+        prevProduct.id === product.id
+          ? {
+              ...prevProduct,
+              selectedQuantity: newQuantity > prevProduct.quantity ? prevProduct.quantity : newQuantity,
+            }
+          : prevProduct
+      )
+    );
+  };
+
+  const { t } = useTranslation();
+  return (
+    <main>
+      <h2 className="text-center">{t('Filt')+val}</h2>
+      <ul className="list-unstyled row">
         {productsNEW.map((product) => (
-          <li key={product.id}>
-            <h3>{product.name}</h3>
-            <img src={product.photo} alt={product.name} />
-            <p>Description: {product.description}</p>
-            <p>Category: {product.category}</p>
-            <p>Brand: {product.brand}</p>
-            <p>Price: ${product.price}</p>
-            <p>Quantity in stock: ${product.quantity}</p>
+          <li key={product.id} className="product-item col-md-6 col-lg-4 col-xl-3 mb-4">
+            <div className="product-image-container">
+              <img src={product.photo} alt={product.name} className="product-image img-fluid" />
+            </div>
+            <h3 className="product-name">{product.name}</h3>
+            <p>{t('description')}{product.description}</p>
+            <p>{t('category')} {product.category}</p>
+            <p>{t('brand')} {product.brand}</p>
+            <p>{t('price')}{product.price}</p>
+            <p>{t('quantitystock')}{product.quantity}</p>
             {product.productParameters.length > 0 && (
               <ul>
                 {product.productParameters.map((parameter) => (
@@ -71,11 +185,34 @@ const SearchpageT = () => {
                 ))}
               </ul>
             )}
+            <label>
+            {t('quantity')}
+              <input
+                type="number"
+                value={product.selectedQuantity}
+                onChange={(e) => handleQuantityChange(product, parseInt(e.target.value))}
+                min={0}
+                max={product.quantity}
+                className="form-control"
+              />
+            </label>
+            <button
+              className="btn btn-success"
+              onClick={() => handleAddToCart(product)}
+            >
+              {t('addtocart')}
+            </button>
+            <button
+          className="btn btn-info"
+          onClick={() => handleAddToWishlist(product)}
+        >
+          {t('addtowish')}
+        </button>
           </li>
         ))}
       </ul>
     </main>
   );
-};
+}
 
 export default SearchpageT;
