96300864040316691d7d1a47b646032721746fd7
Vilius <mano@gmail.lt>
2023-04-24 00:47:48 +0300

    lietuvybe

--- online-shop-react/src/components/UserOrder.js
+++ online-shop-react/src/components/UserOrder.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import './css/product-list.css';
 import CartService from "../services/cart.service";
 import UserService from "../services/user.service";
+import { useTranslation} from 'react-i18next';
 
 function UserOrder() {
   const [inactiveCarts, setInactiveCarts] = useState([]);
@@ -20,10 +21,11 @@ function UserOrder() {
     fetchInactiveCarts();
   }, []);
 
+  const {t} = useTranslation();
   if (inactiveCarts.length === 0) {
     return (
       <main className="container">
-        <h1>No any orders completed.</h1>
+        <h1>{t('noorders')}</h1>
       </main>
     );
   }
@@ -31,27 +33,27 @@ function UserOrder() {
 
   return (
 <main className="container">
-  <h1>Order History:</h1>
+  <h1>{t('orderhistory')}</h1>
   <div className="row">
     {inactiveCarts.map((cart) => (
       <div key={cart.id} className="col-md-4 mb-4">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">Order Number: {cart.id}</h5>
-            <p className="card-text"><b>Username:</b> {cart.user.username}</p>
-            <p><b>Order Total Price:</b> {new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(cart.totalPrice)}</p>
+            <h5 className="card-title">{t('ordernumber')}{cart.id}</h5>
+            <p className="card-text"><b>{t('username1')}</b> {cart.user.username}</p>
+            <p><b>{t('orderprice')}</b> {new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(cart.totalPrice)}</p>
            
             {/* <p className="card-text"><b>Date of Purchase:</b>  </p> */}
             {/*    {cart.items[0].date}  */}
             
-            <h6 className="card-subtitle mb-2 text-muted"><b>Order Items:</b></h6>
+            <h6 className="card-subtitle mb-2 text-muted"><b>{t('orderitems')}</b></h6>
             <ul className="list-group">
             {cart.items.map((item) => (
   <li key={item.id} className="list-group-item">
-    <p><b>Product Name:</b> {item.product.name}</p>
-    <p><b>Product Price:</b> {new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(item.product.price)}</p>
-    <p><b>Quantity:</b> {item.quantity}</p>
-    {item && item.date && <p><b>Date:</b> {item.date}</p>}
+    <p><b>{t('productname')}</b> {item.product.name}</p>
+    <p><b>{t('productprice')}</b> {new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(item.product.price)}</p>
+    <p><b>{t('quantity1')}</b> {item.quantity}</p>
+    {item && item.date && <p><b>{t('date')}</b> {item.date}</p>}
   </li>
 ))}
             </ul>
