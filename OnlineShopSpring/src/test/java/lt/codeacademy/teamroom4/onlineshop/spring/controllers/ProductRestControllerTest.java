0e99fb2422af0bb921373eea69497c52ec8974a4
Vilius <mano@gmail.lt>
2023-04-23 22:18:28 +0300

    testai

--- OnlineShopSpring/src/test/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ProductRestControllerTest.java
+++ OnlineShopSpring/src/test/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/ProductRestControllerTest.java
@@ -1,10 +1,15 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Product;
 
@@ -12,16 +17,25 @@ class ProductRestControllerTest {
 	
 	ProductRestController controller = new ProductRestController();
 	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
 	@Test
 	  public void testSearchByPrice() {
-	    double minPrice = 500.00;
-	    double maxPrice = 1000.00;
-	    @SuppressWarnings("unchecked")
-		List<Product> productList = (List<Product>) controller.search(minPrice, maxPrice);
-	    assertFalse(productList.isEmpty());
-	    for (Product product : productList) {
-	        assertTrue(product.getPrice() >= minPrice && product.getPrice() <= maxPrice);
-	    }
+		double minPrice = 100;
+		double maxPrice = 200;
+		
+		ResponseEntity<Product[]> response = restTemplate.getForEntity("/api/product/search/{min}/{max}", Product[].class, minPrice, maxPrice);
+		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		// Verify that the response body contains products with prices within the specified range
+		    Product[] products = response.getBody();
+		    assertThat(products).isNotNull();
+		    for (Product product : products) {
+		        assertThat(product.getPrice()).isGreaterThanOrEqualTo((minPrice));
+		        assertThat(product.getPrice()).isLessThanOrEqualTo((maxPrice));
+		    }
 	  }
 
 }
--- OnlineShopSpring/src/test/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/WalletControllerTest.java
+++ OnlineShopSpring/src/test/java/lt/codeacademy/teamroom4/onlineshop/spring/controllers/WalletControllerTest.java
@@ -1,42 +1,46 @@
 package lt.codeacademy.teamroom4.onlineshop.spring.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.math.BigDecimal;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
+import lt.codeacademy.teamroom4.onlineshop.spring.entities.User;
 import lt.codeacademy.teamroom4.onlineshop.spring.entities.Wallet;
+import lt.codeacademy.teamroom4.onlineshop.spring.repositories.WalletRepository;
 import lt.codeacademy.teamroom4.onlineshop.spring.services.WalletService;
 
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(WalletController.class)
+//@WebMvcTest(WalletController.class)
+@SpringBootTest
 class WalletControllerTest {
 	
+	
 	 @Autowired
-	 private MockMvc mvc;
-	 @Autowired
-	 
-	 @MockBean
 	 private WalletService walletService;
+	 
+	 @Autowired
+	 WalletRepository walletRepository;
+	 
 	
 	@Test
 	void testGetById() throws Exception {
-		 
-		  }
+		Long id = 10L;
+		Wallet wallet = new Wallet(id, "SEB" ,"454554");
+		walletRepository.save(wallet);
+		if(wallet.getId() == 10) {
+			assertTrue(true);
+		}else {
+			assertTrue(false);
+		}
+		  
 	}
+	
+}
 
 
